ae0cab3d47f4008d2a4968c92bbe6fb3f4e371ec
Connor Hergott <connorhergott@gmail.com>
2018-06-24 14:51:09 -0700

    initial commit

---
+++ index.js.js
@@ -0,0 +1,48 @@
+/*jslint node: true */
+"use strict";
+const fs = require('fs');
+const path = require('path');
+const getFolderSize = require('get-folder-size');
+
+const MOVIE_DIR = `M:/Video/Movies`;
+
+fs.readdir(MOVIE_DIR, (err, folders) => {
+    folders.forEach(folder => {
+
+        let folderDir = path.join(MOVIE_DIR, folder);
+
+        // Remove empty directories
+        // removeEmptyDirectory(folderDir);
+
+        // Rename movie directories with YTS
+        renameMovieFolder(folderDir);
+    });
+
+});
+
+function renameMovieFolder(folderDir) {
+    let name = path.basename(folderDir);
+    let isYTS = /\[YTS.AG\]/ig.test(name);
+    if (isYTS) {
+        let newName = name.replace(' [YTS.AG]', '');
+        let newFolderDir = path.join(path.dirname(folderDir), newName);
+        fs.renameSync(folderDir, newFolderDir);
+        console.log(`* Renamed ${name} to ${newName}`);
+    }
+}
+
+function removeEmptyDirectory(folderDir) {
+    getFolderSize(folderDir, (err, size) => {
+        if (err) console.log('ERROR: ', err);
+
+        if (size === 0) {
+
+            let otherFolder = path.join(folderDir, 'Other');
+            if (fs.existsSync(otherFolder)) {
+                fs.rmdirSync(otherFolder);
+            }
+            fs.rmdirSync(folderDir);
+            console.log('-', file, ' size: ', size, ' bytes');
+        }
+    });
+}
