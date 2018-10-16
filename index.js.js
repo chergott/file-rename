703b69f2171cab34bc74a20497655de2d23b47b3
Connor Hergott <connorhergott@gmail.com>
2018-10-15 21:04:40 -0700

    general file rename

--- index.js.js
+++ index.js.js
@@ -2,57 +2,57 @@
 "use strict";
 const fs = require('fs');
 const path = require('path');
-const getFolderSize = require('get-folder-size');
 
-const DEFAULT_720P_DIR = `M:/Video/Movies`;
-const DEFAULT_1080P_DIR = `R:/Video/Movies`;
+const DIRECTORY = 'C:/Users/CHergott/Downloads/_qBittorrent/makes'
 
-const MOVIE_DIRECTORIES = [DEFAULT_720P_DIR, DEFAULT_1080P_DIR];
+// iterate through each folder in directory
+const folders = fs.readdirSync(DIRECTORY);
 
-MOVIE_DIRECTORIES.forEach(directory => {
-	
-  fs.readdir(directory, (error, folders) => {
-	  
-    if (error) return console.log(error);
+folders.forEach(folderName => {
 
-    folders.forEach(folder => {
+  const folderPath = path.join(DIRECTORY, folderName);
 
-      let folderDir = path.join(directory, folder);
+  const isFolder = fs.lstatSync(folderPath).isDirectory();
+  if (! isFolder) return;
 
-      // Remove empty directories
-      // removeEmptyDirectory(folderDir);
+  // rename files
+  const files = fs.readdirSync(folderPath);
 
-      // Rename movie directories with YTS
-      renameMovieFolder(folderDir);
-    });
+  files.forEach(oldFileName => {
+    const newFileName = toNiceFileName(oldFileName);
 
-  });
-});
+    const oldFilePath = path.join(folderPath, oldFileName);
+    const newFilePath = path.join(folderPath, newFileName);
 
-function renameMovieFolder(folderDir) {
-  let name = path.basename(folderDir);
-  const YTS_REGEX = /\s\[YTS.A\w\]/ig;
-  let isYTS = YTS_REGEX.test(name);
-  if (isYTS) {
-    let newName = name.replace(YTS_REGEX, '');
-    let newFolderDir = path.join(path.dirname(folderDir), newName);
-    fs.renameSync(folderDir, newFolderDir);
-    console.log(`* Renamed ${name} to ${newName}`);
-  }
-}
-
-function removeEmptyDirectory(folderDir) {
-  getFolderSize(folderDir, (err, size) => {
-    if (err) console.log('ERROR: ', err);
-
-    if (size === 0) {
-
-      let otherFolder = path.join(folderDir, 'Other');
-      if (fs.existsSync(otherFolder)) {
-        fs.rmdirSync(otherFolder);
-      }
-      fs.rmdirSync(folderDir);
-      console.log('-', file, ' size: ', size, ' bytes');
+    if (oldFilePath !== newFilePath) {
+      fs.renameSync(oldFilePath, newFilePath);
+      console.log(`- Renamed file ${oldFileName} to ${newFileName}`);
     }
   });
+
+  // rename folder
+  const newFolderName = toNiceFileName(folderName);
+
+  const oldFolderPath = folderPath;
+  const newFolderPath = path.join(path.dirname(folderPath), newFolderName);
+
+  if (oldFolderPath !== newFolderPath) {
+    fs.renameSync(oldFolderPath, newFolderPath);
+    console.log(`Renamed folder ${folderName} to ${newFolderName}`);
+  }
+});
+
+
+function toNiceFileName(filename = '') {
+  filename = cleanFilename(filename);
+  filename = filename.toLowerCase().replace(/[\s_]/, '-');
+  return filename;
+}
+
+function cleanFilename(filename = '') {
+  // remove extra extension if it exists
+  const extension = path.extname(filename);
+  const basename = path.basename(filename, extension).replace(extension, '');
+
+  return `${basename}${extension}`;
 }
